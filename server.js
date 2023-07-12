dae76edf804209f22c507853a5dc47c427384a22
Prehans Gupta <85963909+prehans@users.noreply.github.com>
2023-07-13 01:14:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server =http.createServer(app);
 const io=new Server(server);
 app.use(express.static('build'));
 app.use((req, res , next)=>{
-     res.sendFile(path.resolve(__dirname , 'build' , 'index.html'));
+     res.sendFile(path.join(__dirname , 'build' , 'index.html'));
 });
 
 const userSocketMap={};
